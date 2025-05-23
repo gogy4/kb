89481b231915f6b448ea48f79223bbe24e247f90
Данил <younggogy@gmail.com>
2025-05-23 21:38:59 +0500

    добавил возможность регистрировать новых админов, пофиксил подсчет шансов

---
+++ src/main/java/com/example/demo/controller/AdminRegisterController.java
@@ -0,0 +1,20 @@
+﻿package com.example.demo.controller;
+
+import com.example.demo.application.dto.AdminDto;
+import com.example.demo.application.dto.AdminLoginRequest;
+import com.example.demo.application.service.AdminLoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminRegisterController {
+    private final AdminLoginService adminLoginService;
+
+    @PostMapping("admin/register")
+    public AdminDto register(@RequestBody AdminLoginRequest adminLoginRequest) {
+        return adminLoginService.register(adminLoginRequest.getEmail(), adminLoginRequest.getPassword());
+    }
+}
